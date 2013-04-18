5584cc53211ac4ef5b5047169c72558fee63b2b3
Ian-Woo Kim <ianwookim@gmail.com>
2013-04-18 13:40:01 -0400

    remove comments

--- test/makejson.hs
+++ test/makejson.hs
@@ -177,21 +177,6 @@ wm012 = (p_wm012, wdav_wm012 )
 
 
 
-p_wm0123 :: ProcessSetup SM 
-p_wm0123 = PS {  
-    model = SM
-  , process = [ "p p > w- QCD=99 QED=2 @0"
-              , "p p > w- j QCD=99 QED=2 @1"
-              , "p p > w- j j QCD=99 QED=2 @2"
-              , "p p > w- j j j QCD=99 QED=2 @3"
-              ] 
-  , processBrief = "wm0123j" 
-  , workname   = "wm0123j"
-  }
-
-wdav_wm0123 = WebDAVRemoteDir "montecarlo/admproject/smbkg/wm0123"
-
-wm0123 = (p_wm0123, wdav_wm0123 )
 
 -}
 
@@ -227,21 +212,6 @@ wdav_tbbar012 = WebDAVRemoteDir "montecarlo/admproject/smbkg/tbbar012"
 
 tbbar012 = (p_tbbar012,wdav_tbbar012)
 
-p_tbbar0123 :: ProcessSetup SM 
-p_tbbar0123 = PS {  
-    model = SM
-  , process = [ "p p > t b~ QCD=99 QED=2 @0"
-              , "p p > t b~ j QCD=99 QED=2 @1"
-              , "p p > t b~ j j QCD=99 QED=2 @2"
-              , "p p > t b~ j j j QCD=99 QED=2 @3"
-              ] 
-  , processBrief = "tbbar0123j" 
-  , workname   = "tbbar0123j"
-  }
-
-wdav_tbbar0123 = WebDAVRemoteDir "montecarlo/admproject/smbkg/tbbar0123"
-
-tbbar0123 = (p_tbbar0123,wdav_tbbar0123)
 -}
 
 
@@ -312,6 +282,24 @@ wp0123 = (p_wp0123,wdav_wp0123)
 -}
 
 {-
+p_wm0123 :: ProcessSetup SM 
+p_wm0123 = PS {  
+    model = SM
+  , process = MGProc [] [ "p p > w- QCD=99 QED=2 @0"
+                        , "p p > w- j QCD=99 QED=2 @1"
+                        , "p p > w- j j QCD=99 QED=2 @2"
+                        , "p p > w- j j j QCD=99 QED=2 @3"
+                        ] 
+  , processBrief = "wm0123j" 
+  , workname   = "wm0123j"
+  }
+
+wdav_wm0123 = WebDAVRemoteDir "montecarlo/admproject/smbkg/wm0123"
+
+wm0123 = (p_wm0123, wdav_wm0123 )
+-}
+
+{-
 p_z0123 :: ProcessSetup SM 
 p_z0123 = PS {  
     model = SM
@@ -329,6 +317,7 @@ wdav_z0123 = WebDAVRemoteDir "montecarlo/admproject/smbkg/z0123"
 z0123 = (p_z0123,wdav_z0123)
 -}
 
+{-
 -- | 
 p_tt012 :: ProcessSetup SM 
 p_tt012 = PS {  
@@ -344,7 +333,109 @@ p_tt012 = PS {
 wdav_tt012 = WebDAVRemoteDir "montecarlo/admproject/smbkg/tt012"
 
 tt012 = (p_tt012,wdav_tt012)
+-}
 
+{-
+p_tbbar0123 :: ProcessSetup SM 
+p_tbbar0123 = PS {  
+    model = SM
+  , process = MGProc []  [ "p p > t b~ QCD=99 QED=2 @0"
+                         , "p p > t b~ j QCD=99 QED=2 @1"
+                         , "p p > t b~ j j QCD=99 QED=2 @2"
+                         , "p p > t b~ j j j QCD=99 QED=2 @3"
+                         ] 
+  , processBrief = "tbbar0123j" 
+  , workname   = "tbbar0123j"
+  }
+
+wdav_tbbar0123 = WebDAVRemoteDir "montecarlo/admproject/smbkg/tbbar0123"
+
+tbbar0123 = (p_tbbar0123,wdav_tbbar0123)
+-}
+
+{-
+p_tbarb0123 :: ProcessSetup SM 
+p_tbarb0123 = PS {  
+    model = SM
+  , process = MGProc [] [ "p p > t~ b QCD=99 QED=2 @0"
+                        , "p p > t~ b j QCD=99 QED=2 @1"
+                        , "p p > t~ b j j QCD=99 QED=2 @2"
+                        , "p p > t~ b j j j QCD=99 QED=2 @3"
+                        ] 
+  , processBrief = "tbarb0123j" 
+  , workname   = "tbarb0123j"
+  }
+
+wdav_tbarb0123 = WebDAVRemoteDir "montecarlo/admproject/smbkg/tbarb0123"
+
+tbarb0123 = (p_tbarb0123,wdav_tbarb0123)
+-}
+
+{-
+p_zz012 :: ProcessSetup SM 
+p_zz012 = PS {  
+    model = SM
+  , process = MGProc [] [ "p p > z z QCD=99 QED=2 @0"
+                        , "p p > z z j QCD=99 QED=2 @1"
+                        , "p p > z z j j QCD=99 QED=2 @2"
+                        ] 
+  , processBrief = "zz012j" 
+  , workname   = "zz012j"
+  }
+
+wdav_zz012 = WebDAVRemoteDir "montecarlo/admproject/smbkg/zz012"
+
+zz012 = (p_zz012,wdav_zz012)
+-}
+
+{-
+p_ww012 :: ProcessSetup SM 
+p_ww012 = PS {  
+    model = SM
+  , process = MGProc [] [ "p p > w+ w- QCD=99 QED=2 @0"
+                        , "p p > w+ w- j QCD=99 QED=2 @1"
+                        , "p p > w+ w- j j QCD=99 QED=2 @2"
+                        ] 
+  , processBrief = "ww012j" 
+  , workname   = "ww012j"
+  }
+
+wdav_ww012 = WebDAVRemoteDir "montecarlo/admproject/smbkg/ww012"
+
+ww012 = (p_ww012,wdav_ww012)
+-}
+
+{-
+p_wpz012 :: ProcessSetup SM 
+p_wpz012 = PS {  
+    model = SM
+  , process = MGProc [] [ "p p > w+ z QCD=99 QED=2 @0"
+                        , "p p > w+ z j QCD=99 QED=2 @1"
+                        , "p p > w+ z j j QCD=99 QED=2 @2"
+                        ] 
+  , processBrief = "wpz012j" 
+  , workname   = "wpz012j"
+  }
+
+wdav_wpz012 = WebDAVRemoteDir "montecarlo/admproject/smbkg/wpz012"
+
+wpz012 = (p_wpz012,wdav_wpz012)
+-}
+
+p_wmz012 :: ProcessSetup SM 
+p_wmz012 = PS {  
+    model = SM
+  , process = MGProc [] [ "p p > w- z QCD=99 QED=2 @0"
+                        , "p p > w- z j QCD=99 QED=2 @1"
+                        , "p p > w- z j j QCD=99 QED=2 @2"
+                        ] 
+  , processBrief = "wmz012j" 
+  , workname   = "wmz012j"
+  }
+
+wdav_wmz012 = WebDAVRemoteDir "montecarlo/admproject/smbkg/wmz012"
+
+wmz012 = (p_wmz012,wdav_wmz012)
 
 
 -- | 
@@ -366,33 +457,45 @@ rsetup n = RS { numevent = 50000
               }
 
 
-
-
 main :: IO ()
 main = do 
   -- args <- getArgs 
-  let ns = [1..1000] 
+  let ns = -- ww, wz, zz
+           [1..1000] 
+           ------------ below this tbarb
+           -- [1..1000]
+           -- [468,472,208,32]
+           ------------ below this tbbar
+           -- [1..1000]
+           -- [632,636,60,664,724,768,4,820,888,892,916,936,940,944,952,956,964,992,96,112,132,136,140,144,12,164,204,224,260,276,296,28,392]
+           ------------ below this. w- 
+           -- [3001..10000]
+           -- [1..3000] 
+
+           ------------ below this.  w+
+           -- [4501..10000]
+           -- [3001..4500] 
+           -- [1501..3000]   -- from 1501, each set has 50000 events
+           -- [1..1500] 
+
            ------------ below this.  z
+           -- [8001..10000]
+           -- [6001..8000]
+           -- [4501..6000]  
            -- [3001..4500] 
            -- [1501..3000]
            -- [1..1500]
 
-           ------------ below this.  w+
-           -- [3001..4500] 
-           -- [1501..3000]   -- from 1501, each set has 50000 events
+           ------------ below this. ttbar 
+           -- [1..1000] 
 
-           -- [1201..1500] -- up to set 1500, each set has 100000 events
-           -- [601..1200] 
-           -- [301..600]
 
-           -- concat [[6*n+4,6*n+5,6*n+6] | n <- [0..49] ]  -- -- [1..300] 
-           -- [100,101,102,106,107,108,112,113,114,118,119,120,124,125,126,130,131,132,136,137,138,142,143,144,148,149,150
-      fns012 = map (\n->("testwork" </> "tt012new"++show n++".json")) ns
+      fns012 = map (\n->("testwork" </> "wmz012new"++show n++".json")) ns
       nfns012 = zip ns fns012
       
       nfns012_sp = splitEvery 10 fns012
 
-  mapM_ (mkjson tt012) nfns012
+  mapM_ (mkjson wmz012) nfns012
   mapM_ mkpbs (zip [1..] nfns012_sp)
 
 
